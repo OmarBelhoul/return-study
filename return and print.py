0934ee5e90219a396d76e2a5086d6ca412e373f7
Omar Belhoul <166892687+OmarBelhoul@users.noreply.github.com>
2025-09-27 14:17:10 +0100

    Add files via upload

---
+++ return and print.py
@@ -0,0 +1,28 @@
+def double_print(x):
+    print (x*2)
+
+double_print(5)
+result = double_print(5)
+print(result)
+
+def double_return(y):
+    return y*2
+double_return(5)
+result_2 = double_return(5)
+print(result_2)
+
+def double_returntwo(i):
+    return i*2
+result_3=double_returntwo(4)
+print(result_3)
+print(result_3*3)
+print(result_3+7)
+print(result_3-4)
+
+def square_number(x):
+    return x*x
+
+number = int(input("type a number "))
+result_4 = square_number(number)
+
+print(result_4)
